a96719f4ef50adf49766bbfb2c18ae992bff4217
mrtarikozturk <mr.tarik.ozturk@gmail.com>
2020-12-09 21:25:53 +0100

    Url Notlari

--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,40 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.send('respond with a resource');
 });
 
 module.exports = router;
+
+
+
+
+
+/**
+
+www.w3school.com/home
+.../about
+.../contact
+
+.../products
+.../products/add
+.../products/:id/update
+.../products/update
+.../products/:id/delete
+
+.../categories
+.../categories/add
+.../categories/:id/update
+.../categories/update
+.../categories/:id/delete
+
+.../employees
+.../employees/add
+.../employees/update/:id
+.../employees/update
+.../employees/:id/delete
+
+
+
+*/
